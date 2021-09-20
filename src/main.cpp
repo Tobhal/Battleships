9dc2c81f7bfcf3967e89f75e7ff43686f266e620
Tobias Hallingstad <tobias@hallingstad.me>
2021-09-20 19:59:20 +0200

    change

--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,15 @@
 #include "lobby/lobby.h"
 
 int main() {
-    Pos p(1, 1);
-
-    p *= Pos(1,-1);
-
-    std::cout << p.x << " - " << p.y << std::endl;
-    
-    
-    // Add players
-
     Lobby game1("Lobby 1");
 
-    Player p1("Player 1");
-    Player p2("Player 2");
+    game1.addPlayer(Player("Player 1"));
+    game1.addPlayer(Player("Player 2"));
 
-    game1.addPlayer(p1);
-    game1.addPlayer(p2);
+    for (const auto& [playerName, player] : game1.players) {
+        std::cout << player.name << std::endl;
 
-    // Add players to other players attack board
-
-    // Players place boat
-
-    // Players attack
-
-    // End game
-
+    }
 
     return 0;
 }
